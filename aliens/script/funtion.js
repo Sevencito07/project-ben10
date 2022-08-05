7fa073e877dc13089e2ad02ffb4185b3eedb080c
cortes <cortesseven2003@gmail.com>
2022-08-05 18:19:45 -0500

    Updates

---
+++ aliens/script/funtion.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", ()=>{
+const conte_general =document.querySelector('.conteiner-general')
+conte_general.style.opacity = 0 
+conte_general.style.visibility = 'hidden'
+
+})
